a2da7c21bbcfda86ba48af658ceddb1d0c5dc036
makachikava <chikavamakanaka@gmail.com>
2023-11-30 13:59:26 -0800

    added comments

--- kicksential/product/views.py
+++ kicksential/product/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .serializers import ProductSerializers
-from django.views.generic.edit import UpdateView
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework.response import Response
@@ -8,13 +7,14 @@ from rest_framework.response import Response
 from .models import Product, Category
 from .serializers import ProductSerializers, CategorySerializer, FavoriteSerializer
 
-# Create your views here.
+# -----------GET ALL PRODUCTS ---------- #
 class ProductsList(APIView):
     def get(self, request, format=None):
         products = Product.objects.all()
         serializer = ProductSerializers(products, many=True)
         return Response(serializer.data)
     
+# -----------GET CATEGORIES ----------#
 class CategoryList(APIView):
     def get_object(self, category_slug):
         try:
@@ -27,12 +27,14 @@ class CategoryList(APIView):
         serializer = CategorySerializer(category)
         return Response(serializer.data)
     
+# -------------GET FAVORITES--------------- #
 class FavoritesList(APIView):
     def get(self, request, format=None):
         favorites = Product.objects.filter(favorite = True)
         serializer = FavoriteSerializer(favorites, many=True)
         return Response(serializer.data)
     
+# ---HANDLE GET, PUT, DELETE FOR EACH PRODUCT---- #
 class ProductDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all().order_by('id')
     serializer_class = ProductSerializers
