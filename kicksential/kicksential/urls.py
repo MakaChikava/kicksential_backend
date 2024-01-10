1d021d1e1a67d3f73f61e25a83786fc4ff754dd4
makachikava <chikavamakanaka@gmail.com>
2024-01-09 16:00:30 -0800

    added urls for login and logout

--- kicksential/kicksential/urls.py
+++ kicksential/kicksential/urls.py
@@ -2,10 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from djoser.views import TokenCreateView, TokenDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
+    path('api/v1/', TokenCreateView.as_view(), name='token_create'),  # Include JWT token creation view
+    path('api/v1/', TokenDestroyView.as_view(), name='token_destroy'),  # Include JWT token destruction view
     path('api/v1/', include('product.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
